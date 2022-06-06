c5a7d6cf397cda8605f4389064a6221d8f152a7b
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-06-06 14:53:33 -0600

    none

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 const userInput = document.querySelector('.inputTodo');
 const list = document.querySelector('.todoList')
-/*const button = userInput.addEventListener('submit', e => {
-    e.preventDefault();
-    const todo = userInput.add.value;
-    console.log(todo);
 
-});//document.querySelector('.todoButton');//
-*/
+//create todo list
 const makeTodo = todo => {
     const html =
         `<li class="list">
@@ -17,74 +12,18 @@ const makeTodo = todo => {
     list.innerHTML += html;
 };
 
-
+//create todo from user input
 userInput.addEventListener('submit', e => {
     e.preventDefault();
     const todo = userInput.add.value.trim();
-    //console.log(todo);
     userInput.reset();
 
     makeTodo(todo);
 });
 
-//delete
+//delete todo from list
 list.addEventListener('click', e => {
     if (e.target.classList.contains('fa-trash-can')) {
         e.target.parentElement.remove();
     }
 });
-/*button.addEventListener('click', (e) => {
-    e.preventDefault();
-    //const todo = userInput.todoInput.value;
-    console.log(todo);
-});
-*/
-
-//userInput.value = 'ninjas are not awesome.'
-//console.log(userInput);
-//console.log(button);
-//function inputToDo()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function displayTodo() {
-    const inputElement = document.getElementById("todo-input");
-    const text = inputElement.value;
-    const todoList = document.getElementById("todo-list");
-    const todo = createTodo(text);
-
-    todoList.append(todo);
-    inputElement.value = "";
-
-}
-
-function createTodo(text) {
-    const todoListItem = document.createElement("li");
-    const todoListItemDelete = document.createElement("button");
-    todoListItemDelete.className = 'btn btn-success btn-sm float-right delete col-2 '
-    todoListItemDelete.appendChild(document.createTextNode('Delete'))
-
-    //todoListItemDelete.setAttribute("id", "deleteButton");
-    //todoListItemDelete.innerText = "Delete";
-
-
-
-    todoListItem.append(text, todoListItemDelete);
-
-    return todoListItem;
-}
-
-document.getElementById("add-todo")
-    .addEventListener("click", displayTodo)
-    */
